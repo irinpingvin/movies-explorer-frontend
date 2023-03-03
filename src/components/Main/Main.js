0502d6ff805cd56fcdc8edff53c3fddba8eb5112
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-03-04 01:18:29 +0300

    Дипломная работа
    Реализован компонент AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
+import AboutProject from "../AboutProject/AboutProject";
 
 function Main() {
   return (
     <>
       <Promo/>
+      <AboutProject/>
     </>
   );
 }
