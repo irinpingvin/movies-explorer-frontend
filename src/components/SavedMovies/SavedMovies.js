75f19d77a420a2e352cfcf8e7f41b47d0220416e
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-03-07 02:45:42 +0300

    Дипломная работа
    Реализован компонент SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,16 +1,12 @@
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
-import Preloader from "../Preloader/Preloader";
-import More from "../More/More";
 
 function SavedMovies(props) {
   return (
-    <>
+    <div className="saved-movies">
       <SearchForm onSearchForm={props.onSearchForm}/>
       <MoviesCardList cards={props.cards} onCardSaved={props.onCardSaved} isSavedMode={true}/>
-      {/*<Preloader/>*/}
-      <More cards={props.cards}/>
-    </>
+    </div>
   );
 }
 
