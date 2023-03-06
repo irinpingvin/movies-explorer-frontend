56118ca510893a474e500a5c603fdc5f185dba73
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-03-06 03:07:00 +0300

    Дипломная работа
    Добавлен компонент Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,13 @@
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import Preloader from "../Preloader/Preloader";
 
 function Movies(props) {
   return (
     <>
       <SearchForm onSearchForm={props.onSearchForm}/>
       <MoviesCardList cards={props.cards} onCardSaved={props.onCardSaved}/>
+      <Preloader/>
     </>
   );
 }
