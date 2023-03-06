0fd338c094a1b8d7827cbfffba5fe5971bbbeecd
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-03-07 01:34:26 +0300

    Дипломная работа
    Реализована вёрстка компонента More

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,15 @@
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Preloader from "../Preloader/Preloader";
+import More from "../More/More";
 
 function Movies(props) {
   return (
     <>
       <SearchForm onSearchForm={props.onSearchForm}/>
       <MoviesCardList cards={props.cards} onCardSaved={props.onCardSaved}/>
-      <Preloader/>
+      {/*<Preloader/>*/}
+      <More cards={props.cards}/>
     </>
   );
 }
