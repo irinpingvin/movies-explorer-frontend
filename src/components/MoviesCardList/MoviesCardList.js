48c564f126dedf42894d84cbbfd6540946cfe9b9
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-03-18 02:15:08 +0300

    Дипломная работа
    Исправлена ошибка рендера сохранённых фильмов

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,7 +4,7 @@ function MoviesCardList(props) {
   return (
     <section className="movies">
       <ul className="movies-list">
-        {props.movies.map((card) => <MoviesCard key={card.id} card={card} savedCards={props.savedMovies} onCardSave={props.onMovieSave} isSavedMode={props.isSavedMode}/>)}
+        {props.movies.map((card) => <MoviesCard key={props.isSavedMode ? card._id : card.id} card={card} savedCards={props.savedMovies} onCardSave={props.onMovieSave} isSavedMode={props.isSavedMode}/>)}
       </ul>
     </section>
   );
