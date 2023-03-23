51b18c1ec5e8ebfbfaccb66c00b2a2e4f04cc0ee
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-03-23 21:12:49 +0300

    Дипломная работа
    Актуализация базового урла

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
-/*const MAIN_API_URL = 'https://api.movies-explr.alyabeva.nomoredomains.work';*/
-const MAIN_API_URL = 'http://localhost:3001';
+const MAIN_API_URL = 'https://api.movies-explr.alyabeva.nomoredomains.work';
 const HEADERS = {
   'Content-Type': 'application/json'
 };
